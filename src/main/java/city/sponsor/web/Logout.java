b086404d438a2a9277389cf0656c0740f1b1d780
sibow <sibow@bloomington.in.gov>
2023-01-05 13:14:04 -0500

    first commit

---
+++ src/main/java/city/sponsor/web/Logout.java
@@ -0,0 +1,104 @@
+package city.sponsor.web;
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import city.sponsor.model.*;
+import city.sponsor.list.*;
+import city.sponsor.util.*;
+/**
+ *
+ *
+ *
+ */
+@WebServlet(urlPatterns = {"/Logout"})
+public class Logout extends TopServlet{
+
+    static final long serialVersionUID = 48L;	
+    /**
+     * Deletes the sesion info.
+     * @param req
+     * @param res
+     */
+    public void doGet(HttpServletRequest req, 
+		      HttpServletResponse res) 
+	throws ServletException, IOException{
+
+	res.setContentType("text/html");
+	PrintWriter out = res.getWriter();
+	Enumeration<String> values = req.getParameterNames();
+	String name= "";
+	String value = "";
+	String switchTo = "";
+	String id = "";
+	while (values.hasMoreElements()) {
+	    name = values.nextElement().trim();
+	    value = req.getParameter(name).trim();
+	    if (name.equals("id"))
+		id = value;	
+	}	
+	HttpSession session = null;
+	session = req.getSession(false);
+	if(session != null){
+	    session.invalidate();
+
+	}
+	res.sendRedirect(url+"Login");	 			
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/city/sponsor/web/TopServlet.java
@@ -0,0 +1,66 @@
+package city.sponsor.web;
+import java.net.URI;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import city.sponsor.util.*;
+
+public class TopServlet extends HttpServlet {
+    static String url = "",url2="", server_path="";
+    static boolean debug = false;
+    static boolean activeMail = false;
+    static String emailStr="@bloomington.in.gov";
+    static String cityStateZip = "Bloomington, IN 47402",
+	pobox="848", phones="(812)349-3739", sender="";
+    static String defaultAttention = "";
+    // for now 
+    static Configuration config = null;
+    static Logger logger = LogManager.getLogger(TopServlet.class);
+    static ServletContext context = null;
+    public void init(ServletConfig conf){
+	try{
+	    context = conf.getServletContext();
+	    url = context.getInitParameter("url");
+	    url2 = context.getInitParameter("url2");
+	    server_path = context.getInitParameter("server_path");
+	    String str = context.getInitParameter("debug");
+	    if(str != null && str.equals("true")) debug = true;
+	    str = context.getInitParameter("activeMail");
+	    if(str != null && str.equals("true")) activeMail = true;	    
+	    str = context.getInitParameter("emailStr");
+	    if(str != null && !str.isEmpty()) emailStr = "@"+str;
+	    str = context.getInitParameter("cityStateZip");
+	    if(str != null && !str.isEmpty()) cityStateZip = str;
+	    str = context.getInitParameter("pobox");
+	    if(str != null && !str.isEmpty()) pobox = str;
+	    str = context.getInitParameter("phones");
+	    if(str != null && !str.isEmpty()) phones = str;
+	    str = context.getInitParameter("attention");
+	    if(str != null) defaultAttention = str;
+	    str = context.getInitParameter("sender");
+	    if(str != null) sender=str;
+	    String username = context.getInitParameter("adfs_username");
+	    String auth_end_point = context.getInitParameter("auth_end_point");
+	    String token_end_point = context.getInitParameter("token_end_point");
+	    String callback_uri = context.getInitParameter("callback_uri");
+	    String client_id = context.getInitParameter("client_id");
+	    String client_secret = context.getInitParameter("client_secret");
+	    String scope = context.getInitParameter("scope");
+	    String discovery_uri = context.getInitParameter("discovery_uri");
+	    config = new
+		Configuration(auth_end_point, token_end_point, callback_uri, client_id, client_secret, scope, discovery_uri, username);
+	    // System.err.println(config.toString());
+	}catch(Exception ex){
+	    System.err.println(" top init "+ex);
+	    logger.error(" "+ex);
+	}
+    }
+
+}
